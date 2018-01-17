9bfc57260e4c5f5f93acbae93057b9c548553902
Anubhav Chaudhary <achaudhary997@yahoo.in>
2018-01-17 22:04:13 +0530

    Adding to cart works, items now loads in cart (not formatted)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import decimal
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -10,25 +12,9 @@ class Item(models.Model):
     description = models.TextField()
     image = models.FileField()
 
-    def total_price(self):
-        return self.quantity * self.unit_price
-
-    total_price = property(total_price)
-
 
 class Cart(models.Model):
-    items = models.ManyToManyField(Item)
-    num_of_items = models.IntegerField()
-    total_price = models.IntegerField()
+    item_id = models.IntegerField()
     checked_out = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def add_item(self, item):
-        self.items.add(item)
-        self.num_of_items += 1
-        self.total_price += item.unit_price * item.quantity
-
-    def remove_item(self, item):
-        self.items.remove(item)
-        self.num_of_items -= 1
-        self.total_price -= item.unit_price * item.quantity
--- main/views.py
+++ main/views.py
@@ -4,25 +4,28 @@ from main.models import Item, Cart
 
 
 def index(request):
-
     return render(request, 'main/index.html', {"item": Item.objects.all().first()})
 
 
-def add_to_cart(request, itemID):
-    if request.user.cart_set.all().exists():
-        cart = request.user.cart_set.get(user_id=request.user.id)
+def cart(request):
+    if request.user.is_authenticated:
+        # cartItems = []
+        item_ids = Cart.objects.filter(user_id=request.user.id)
+        # for i in item_ids:
+        cartItems = [Item.objects.get(pk=i.item_id) for i in item_ids]
+            # print (i.item_id, type(i.item_id))
+            # cartItems.append(Item.objects.get(pk=i))
+        return render(request, 'main/cart.html', {"items_in_cart": cartItems})
     else:
+        return redirect('/login/')
+
+def add_to_cart(request, itemID):
+    if request.user.is_authenticated:
         cart = Cart()
         cart.user_id = request.user.id
-        cart.num_of_items = 0
-        cart.total_price = 0.0
+        cart.item_id = Item.objects.get(pk=itemID).id
         cart.save()
-    list_of_item = Item.objects.all()
-    for item in list_of_item:
-        if item.id == itemID:
-            cart.add_item(item)
-            # cart.items.add(item)
-            # cart.num_of_items += 1
-            # cart.total_price += item.unit_price * item.quantity
-            break
-    return redirect('/')
+
+        return redirect('/')
+    else:
+        return redirect('/login/')
