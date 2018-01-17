cdc38d5fedd3d35539d3c18eaa66d84a82ea4f5d
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-01-17 19:21:18 +0530

    Logout Done

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name="index"),
     path('login/', auth_views.login, name="login"),
+    path('logout/', auth_views.logout, name="logout"),
     path('cart/', views.index, name="cart"),
     path('checkout/', views.index, name="checkout"),
     path('wishlist/', views.index, name="wishlist"),
--- main/views.py
+++ main/views.py
@@ -6,5 +6,3 @@ def index(request):
     return render(request, 'main/index.html', {"custom_notifications": ""})
 
 
-def login(request):
-    return render(request, 'main/login.html', context=None)
