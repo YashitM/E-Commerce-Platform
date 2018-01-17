085248da56f1a3177f0866c91c018778dca8246c
Anubhav Chaudhary <achaudhary997@yahoo.in>
2018-01-17 18:20:04 +0530

    added Item class

--- main/models.py
+++ main/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
 class Cart(models.Model):
     num_of_items = models.IntegerField()
     total_price = models.IntegerField()
+    checked_out = models.BooleanField(default=False)
+
+
+class Item(models.Model):
+    cart = models.ForeignKey(Cart)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
+    object_id = models.PositiveIntegerField()
+
+    def total_price(self):
+        return self.quantity*self.unit_price
+    total_price = property(total_price)
+
+    
